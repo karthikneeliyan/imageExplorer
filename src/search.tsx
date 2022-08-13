6ea5e64c5e6cce92a2bf2f720b7ac00ec1252822
kneeli027 <kneeli027@apac.comcast.com>
2022-08-13 20:06:32 +0530

    add fa icons

--- src/search.tsx
+++ src/search.tsx
@@ -1,27 +1,33 @@
 import { useContext, useEffect, useState, useRef } from "react";
-
+import { FaSearch } from "react-icons/fa";
+async function fetchurl(val: any) {
+  const urls = await fetch(
+    `https://pixabay.com/api/?key=29218888-86f75d770c8acd96879f0be50&q=${val}&per_page=200`
+  );
+  const res: any = await urls.json();
+  console.log(res.hits);
+  return res.hits;
+}
 export const SearchBox = ({ setImages }: { setImages: any }) => {
   const [url, setURL] = useState("");
   const inputref = useRef<any>();
 
   useEffect(() => {}, []);
   const handleChange = (e: any) => {
-    async function fetchurl(val: any) {
-      const urls = await fetch(
-        `https://pixabay.com/api/?key=29218888-86f75d770c8acd96879f0be50&q=${val}&per_page=200`
-      );
-      const res: any = await urls.json();
-      console.log(res.hits);
-      setImages(res.hits);
-    }
     if (e.keyCode == 13) {
       if (inputref.current) {
         inputref.current.blur();
-
-        fetchurl(e.target.value);
+        (async () => {
+         const images= await fetchurl(e.target.value);
+          setImages(images)
+        })();
       }
     }
     setURL(e.target.value);
+  };
+  const handleSubmit = async () => {
+    const images = await fetchurl(url);
+    setImages(images);
   };
 
   return (
@@ -34,6 +40,9 @@ export const SearchBox = ({ setImages }: { setImages: any }) => {
         onChange={handleChange}
         value={url}
       />
+      <span>
+        <FaSearch onClick={handleSubmit} className="searchicon" />
+      </span>
     </div>
   );
 };
